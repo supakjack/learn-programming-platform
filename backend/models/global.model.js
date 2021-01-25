f9084ee5379c5adb6320ba30a493a2ba89b935b5
niphitphon8bit <niphitphon_nt@hotmail.com>
2021-01-25 20:54:13 +0700

    Update  - function update to dynamic condition passing and dynamic update

--- backend/models/global.model.js
+++ backend/models/global.model.js
@@ -1,56 +1,65 @@
-const knex = require('./../helpers/init_knex')
-const createError = require('http-errors')
+const knex = require("./../helpers/init_knex");
+const createError = require("http-errors");
 
 module.exports = {
   insert: async (table) => {
     return new Promise((resolve, reject) => {
       try {
-        const doseInsert = knex(table.name).insert(table.insertData)
-        resolve(doseInsert)
+        const doseInsert = knex(table.name).insert(table.insertData);
+        resolve(doseInsert);
       } catch (error) {
-        console.log(error.message)
-        reject(createError.InternalServerError())
+        console.log(error.message);
+        reject(createError.InternalServerError());
       }
-    })
+    });
   },
   select: async (table) => {
     return new Promise((resolve, reject) => {
       try {
-        const doseSelect = knex.select(table.filter).from(table.name)
+        const doseSelect = knex.select(table.filter).from(table.name);
         if (table.condition) {
           table.condition.forEach((condition) => {
-            doseSelect.where(condition)
-          })
+            doseSelect.where(condition);
+          });
         }
-        resolve(doseSelect)
+        resolve(doseSelect);
       } catch (error) {
-        console.log(error.message)
-        reject(createError.InternalServerError())
+        console.log(error.message);
+        reject(createError.InternalServerError());
       }
-    })
+    });
   },
   update: async (table) => {
     return new Promise((resolve, reject) => {
       try {
-        const doseUpdate = knex(table.name)
-          .where(table.condition)
-          .update(table.updateData)
-        resolve(doseUpdate)
+        const doseUpdate = knex(table.name);
+        if (table.condition) {
+          table.condition.forEach((condition) => {
+            doseUpdate.where(condition);
+          });
+          if (table.updateData) {
+            table.updateData.forEach((updateData) => {
+              doseUpdate.update(updateData);
+            });
+          }
+        }
+
+        resolve(doseUpdate);
       } catch (error) {
-        console.log(error.message)
-        reject(createError.InternalServerError())
+        console.log(error.message);
+        reject(createError.InternalServerError());
       }
-    })
+    });
   },
   delete: async (table) => {
     return new Promise((resolve, reject) => {
       try {
-        const doseDelete = knex(table.name).where(table.condition).del()
-        resolve(doseDelete)
+        const doseDelete = knex(table.name).where(table.condition).del();
+        resolve(doseDelete);
       } catch (error) {
-        console.log(error.message)
-        reject(createError.InternalServerError())
+        console.log(error.message);
+        reject(createError.InternalServerError());
       }
-    })
-  }
-}
+    });
+  },
+};
