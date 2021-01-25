c48232679e1f991a87d735f340c2683eacbc1cbc
niphitphon8bit <niphitphon_nt@hotmail.com>
2021-01-25 16:51:49 +0700

    init tag from test-service project to LPP project

--- backend/controllers/tag.controller.js
+++ backend/controllers/tag.controller.js
@@ -1,11 +1,134 @@
-const createError = require('http-errors')
-
+const createError = require("http-errors");
+const tagsModel = require("../models/tag");
 module.exports = {
-  foo : async (req, res, next) => {
+  // function name: create
+  // description: create tag by API
+  // input: tagName, tagStatus, tagCreateby , tagUpdateBy
+  // output: text response
+  // CreateBy: Niphitphon Thanatkulkit / CreateDate: 14/1/2021
+  // UpdateBy: Niphitphon Thanatkulkit / UpdateDate: 19/1/2021
+  create: async (req, res, next) => {
+    // define variable json as object
+    var jsonData = {};
+
+    // passing data to json variable
+    jsonData.tagName = req.body.tagName;
+    jsonData.tagStatus = req.body.tagStatus;
+
+    // passing Create and update by tagCreateBy
+    jsonData.tagCreateBy = req.params.tagCreateBy;
+    jsonData.tagUpdateBy = req.params.tagCreateBy;
+
+    // try call function createTag in tags model then catch if error
     try {
-      
+      const doseCreate = await tagsModel.createTag(jsonData);
+      res.status(201).send({ doseCreate });
     } catch (error) {
-      
+      if (error.isJoi === true) return next(createError.InternalServerError());
+      next(error);
     }
-  }
-}
+  },
+
+  // function name: update
+  // desxription: update tag by API from tagId
+  // input: tagId
+  // output: text response
+  // CreateBy: Niphitphon Thanatkulkit / CreateDate: 14/1/2021
+  // UpdateBy: Niphitphon Thanatkulkit / UpdateDate: 15/1/2021
+  update: async (req, res, next) => {
+    try {
+      const message = "update_tag: route allowed";
+      res.send({ message });
+    } catch (error) {
+      if (error.isJoi === true) return next(createError.InternalServerError());
+      next(error);
+    }
+  },
+
+  // function name: delete
+  // description: delete tag by API from create by id
+  // input: tagId
+  // output: text response
+  // CreateBy: Niphitphon Thanatkulkit / CreateDate: 14/1/2021
+  // UpdateBy: Niphitphon Thanatkulkit / UpdateDate: 19/1/2021
+  delete: async (req, res, next) => {
+    // define variable json as object
+    var jsonData = {};
+
+    // passing data to json variable
+    jsonData.tagId = req.params.tagId;
+    jsonData.tagCreateBy = req.params.tagCreateBy;
+
+    // try call function deleteTag in tags model then catch if error
+    try {
+      const doseDelete = await tagsModel.deleteTag(jsonData);
+      res.status(200).send({ doseDelete });
+    } catch (error) {
+      if (error.isJoi === true) return next(createError.InternalServerError());
+      next(error);
+    }
+  },
+
+  // function name: getAll
+  // description: get all tag
+  // input : -
+  // output : all tag in database
+  // CreateBy: Niphitphon Thanatkulkit / CreateDate: 14/1/2021
+  // UpdateBy: Niphitphon Thanatkulkit / UpdateDate: 19/1/2021
+  getAll: async (req, res, next) => {
+    // try call function getAlltag in tags model then catch if error
+    try {
+      const doseGetAll = await tagsModel.getAlltag();
+      res.status(200).send({ doseGetAll });
+    } catch (error) {
+      if (error.isJoi === true) return next(createError.InternalServerError());
+      next(error);
+    }
+  },
+
+  // function name: getTag
+  // description: get single tag
+  // input : tagId
+  // output : tagId, tagName, tagStatus, tagCreateDate, tagUpdateDate, tagCreateBy, tagUpdateBy
+  // CreateBy: Niphitphon Thanatkulkit / CreateDate: 14/1/2021
+  // UpdateBy: Niphitphon Thanatkulkit / UpdateDate: 19/1/2021
+  getTag: async (req, res, next) => {
+    // define variable json as object
+    var jsonData = {};
+
+    // passing data to json variable
+    jsonData.tagId = req.params.tagId;
+    jsonData.tagCreateBy = req.params.tagCreateBy;
+
+    // try call function getTagById in tags model then catch if error
+    try {
+      const doseGetAll = await tagsModel.getTagById(jsonData);
+      res.status(201).send({ doseGetAll });
+    } catch (error) {
+      if (error.isJoi === true) return next(createError.InternalServerError());
+      next(error);
+    }
+  },
+
+  // function name: getAllById
+  // description: get all tag by tagCreateBy
+  // input : tagCreateBy
+  // output : all tag create by tagCreateBy
+  // CreateBy: Niphitphon Thanatkulkit / CreateDate: 14/1/2021
+  // UpdateBy: Niphitphon Thanatkulkit / UpdateDate: 19/1/2021
+  getAlltagById: async (req, res, next) => {
+    // define variable json as object
+    var jsonData = {};
+
+    // passing data to json variable
+    jsonData.tagCreateBy = req.params.tagCreateBy;
+    // try call function getAlltagById in tags model then catch if error
+    try {
+      const doseGetAllById = await tagsModel.getAlltagById(jsonData);
+      res.status(200).send({ doseGetAllById });
+    } catch (error) {
+      if (error.isJoi === true) return next(createError.InternalServerError());
+      next(error);
+    }
+  },
+};
--- backend/routers/tag.router.js
+++ backend/routers/tag.router.js
@@ -1,8 +1,23 @@
-const express = require('express')
-const router = express.Router()
-const tagController = require('./../controllers/tag.controller')
-const timeout = require('connect-timeout')
+const express = require("express");
+const router = express.Router();
+const tagController = require("../controllers/tag.controller");
 
-router.get('/foo', timeout('5s'), tagController.foo)
+// Create a new tag by tagCreateBy
+router.post("/api/:tagCreateBy/create/", tagController.create);
 
-module.exports = router
+// Update a tag by tagCreateBy
+router.get("/api/:tagCreateBy/update/:tagId", tagController.update);
+
+// Delete a tag by tagCreateBy
+router.delete("/api/:tagCreateBy/delete/:tagId", tagController.delete);
+
+// Get all tag by tagCreateBy
+router.get("/api/:tagCreateBy/getAll/", tagController.getAlltagById);
+
+// Get all tags
+router.get("/api/getAll/", tagController.getAll);
+
+// Get single tag
+router.get("/api/:tagCreateBy/getTag/:tagId", tagController.getTag);
+
+module.exports = router;
