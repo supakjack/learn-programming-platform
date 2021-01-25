da0719ece79c92d8e0ef1d37af7d6d8a9225039a
niphitphon8bit <niphitphon_nt@hotmail.com>
2021-01-25 20:49:08 +0700

    change router to single route by condition passing

--- backend/routers/tag.router.js
+++ backend/routers/tag.router.js
@@ -3,21 +3,12 @@ const router = express.Router();
 const tagController = require("../controllers/tag.controller");
 
 // Create a new tag by tagCreateBy
-router.post("/api/:tagCreateBy/create/", tagController.create);
+router.post("/", tagController.create);
 
-// Update a tag by tagCreateBy
-router.get("/api/:tagCreateBy/update/:tagId", tagController.update);
+// update a tag by condition
+router.patch("/", tagController.update);
 
-// Delete a tag by tagCreateBy
-router.delete("/api/:tagCreateBy/delete/:tagId", tagController.delete);
-
-// Get all tag by tagCreateBy
-router.get("/api/:tagCreateBy/getAll/", tagController.getAlltagById);
-
-// Get all tags
-router.get("/api/getAll/", tagController.getAll);
-
-// Get single tag
-router.get("/api/:tagCreateBy/getTag/:tagId", tagController.getTag);
+// Get tag by condition
+router.get("/", tagController.get);
 
 module.exports = router;
