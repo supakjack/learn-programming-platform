3636920e716fa696e1a139de2ce3ccc443574047
niphitphon8bit <niphitphon_nt@hotmail.com>
2021-01-25 20:53:10 +0700

    Add  - createTagSchema  - getTagSchema  - updateTagConditionSchema  - updateTagSchema

--- backend/helpers/validation.helper.js
+++ backend/helpers/validation.helper.js
@@ -1,10 +1,43 @@
-const joi = require('@hapi/joi')
+const joi = require("@hapi/joi");
 
 const authSchema = joi.object({
   username: joi.string().lowercase().required(),
-  password: joi.string().min(2).required()
-})
+  password: joi.string().min(2).required(),
+});
+
+// create tag schema
+const createTagSchema = joi.object({
+  tagName: joi.string().max(255).required(),
+  tagStatus: joi.number().integer().max(3).required(),
+  tagCreateBy: joi.number().integer().max(99999999).min(1).required(),
+  tagUpdateBy: joi.number().integer().max(99999999).min(1).required(),
+});
+
+// get tag schema
+const getTagSchema = joi.object({
+  tagId: joi.number().max(8),
+  tagCreateBy: joi.number().integer().max(99999999).min(1),
+});
+
+// filter update params schema
+const updateTagConditionSchema = joi.object({
+  tagId: joi.number().integer().max(99999999),
+});
+
+// update tag schema
+const updateTagSchema = joi.object({
+  tagId: joi.number().integer().max(99999999),
+  tagName: joi.string().max(255),
+  tagUpdateDate: joi.date().timestamp(),
+  tagUpdateBy: joi.number().max(99999999).min(1),
+  tagCreateBy: joi.number().max(99999999).min(1),
+  tagStatus: joi.number().integer().max(3),
+});
 
 module.exports = {
-  authSchema
-}
+  authSchema,
+  createTagSchema,
+  getTagSchema,
+  updateTagConditionSchema,
+  updateTagSchema,
+};
