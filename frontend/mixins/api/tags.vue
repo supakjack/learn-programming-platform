8967134b40547258d082e78c27d0dbd2c2bec4a8
niphitphon8bit <niphitphon_nt@hotmail.com>
2021-01-31 20:14:53 +0700

    add  - component    - problem    - tags - mixin api    - tags

---
+++ frontend/mixins/api/tags.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+  methods: {
+    async getTag() {
+      this.response = await this.$axios.$get("/tag");
+      console.log(this.response.doseGetAll)
+    }
+  }
+};
+</script>
