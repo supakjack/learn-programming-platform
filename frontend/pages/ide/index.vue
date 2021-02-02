27828acac0edca99a3d2b9f14b0fd7181b733e8d
Theo <theozaza@hotmail.com>
2021-02-03 00:28:47 +0700

    create front ide

---
+++ frontend/pages/ide/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-card class="kanit-font">
+    <v-tabs centered v-model="tab">
+      <v-tab href="#problem">IDE</v-tab>
+      <v-tab href="#tag">แท็ก</v-tab>
+    </v-tabs>
+
+    <v-tabs-items :value="tab">
+      <v-tab-item value="problem">
+        <div>
+          <problem />
+        </div>
+      </v-tab-item>
+      <v-tab-item value="tag">
+        <div>
+          <tags />
+        </div>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-card>
+</template>
+
+<script>
+import tags from "./../problem-tag/tags/index";
+import problems from "./../problem-tag/problems/index";
+// import problem from "./../../components/problem";
+import tagsmixin from "../../components/tags";
+export default {
+  mixins: [tagsmixin],
+  computed: {
+    tab: {
+      set(tab) {
+        this.$router.replace({ query: { ...this.$route.query, tab } });
+      },
+      get() {
+        return this.$route.query.tab;
+      }
+    }
+  },
+  components: {
+    tags,
+    problems
+  }
+
+  //   layout: "modren"
+};
+</script>
+
+<style>
+.roboto-font {
+  font-family: "Roboto", sans-serif;
+}
+.kanit-font {
+  font-family: "Kanit", sans-serif;
+}
+</style>
