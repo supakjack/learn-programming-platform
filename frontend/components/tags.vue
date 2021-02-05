0646f1469371426065f4687064edbe466b68be07
niphitphon8bit <niphitphon_nt@hotmail.com>
2021-02-05 17:28:58 +0700

    update  - insertTag(data)     - passing value     - recieve response  - editTag(data)     - passing value     - recieve response

--- frontend/components/tags.vue
+++ frontend/components/tags.vue
@@ -7,11 +7,34 @@ export default {
     async deleteTag() {
       return this.$axios.$patch("/tag");
     },
-    async editTag() {
-      return this.$axios.$patch("/tag");
+    async editTag(data) {
+     
+      const UpdateData = {};
+      UpdateData.tagName = data.tagName;
+      UpdateData.tagUpdateBy = data.tagUpdateBy;
+      UpdateData.tagStatus = data.tagStatus;
+      UpdateData.tagUpdateDate = data.tagUpdateDate;
+  
+      return this.$axios
+        .$patch(
+          `/tag/?tagId=${data.tagId}`,
+          UpdateData
+        )
+        .catch(err => console.log(err))
+        .then(response => response.doesUpdate);
     },
-    async insertTag() {
-      return this.$axios.$post("/tag");
+    async insertTag(data) {
+      const InsertData = {};
+      InsertData.tagName = data.tagName;
+      InsertData.tagStatus = data.tagStatus;
+      InsertData.tagCreateBy = data.tagCreateBy;
+      InsertData.tagUpdateBy = data.tagUpdateBy;
+      return this.$axios
+        .$post("/tag", InsertData)
+        .catch(error => {
+          console.error(error);
+        })
+        .then(response => response.doesCreate);
     }
   }
 };
