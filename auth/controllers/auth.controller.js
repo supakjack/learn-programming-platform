7d17778c9b61b8a3d6a89d6d251c72d03b8d6c3c
Theo <theozaza@hotmail.com>
2021-02-11 02:00:12 +0700

    .

--- auth/controllers/auth.controller.js
+++ auth/controllers/auth.controller.js
@@ -1,24 +1,24 @@
-const createError = require('http-errors')
-const jwt = require('jsonwebtoken')
-const passport = require('passport')
+const createError = require("http-errors");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
 
 module.exports = {
   login: async (req, res, next) => {
-    console.log(req.body)
+    console.log(req.body);
     try {
-      passport.authenticate('local', { session: false }, (err, user, info) => {
-        if (err) return next(err)
+      passport.authenticate("local", { session: false }, (err, user, info) => {
+        if (err) return next(err);
         if (user) {
-          const token = jwt.sign(user, 'your_jwt_secret')
-          return res.json({ user, token })
+          const token = jwt.sign(user, "your_jwt_secret");
+          return res.json({ user, token });
         } else {
-          return res.status(422).json(info)
+          return res.status(422).json(info);
         }
-      })(req, res, next)
+      })(req, res, next);
     } catch (error) {
       if (error.isJoi === true)
-        return next(createError.BadRequest('Invalid Username/Password'))
-      next(error)
+        return next(createError.BadRequest("Invalid Username/Password"));
+      next(error);
     }
-  }
-}
+  },
+};
