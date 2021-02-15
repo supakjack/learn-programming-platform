633eaa26d92dbdaa5a6be530f13983c7c27e2846
phukeiei <34035927+phukeiei@users.noreply.github.com>
2021-02-15 12:17:00 +0700

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -56,63 +56,64 @@ module.exports = {
       next(error);
     }
   },
-  file: async (req, res, next) => {
-    const singleFile = req.files ? req.files.excelFile : null;
+  file: async (req, res, next) => {},
+  // file: async (req, res, next) => {
+  //   const singleFile = req.files ? req.files.excelFile : null;
 
-    // console.log(req.files);
-    const randomFileName = nanoid(10);
-    const splitFileName = singleFile.name.split(".");
-    // console.log(randomFileName);
-    // console.log(splitFileName);
-    singleFile.name = randomFileName + "." + splitFileName[1];
-    // console.log(singleFile.name);
-    const filePath = process.env.BASE_STORAGE_PATH + "temp";
-    await singleFile.mv(filePath + "\\" + singleFile.name);
-    // console.log(singleFile);
+  //   // console.log(req.files);
+  //   const randomFileName = nanoid(10);
+  //   const splitFileName = singleFile.name.split(".");
+  //   // console.log(randomFileName);
+  //   // console.log(splitFileName);
+  //   singleFile.name = randomFileName + "." + splitFileName[1];
+  //   // console.log(singleFile.name);
+  //   const filePath = process.env.BASE_STORAGE_PATH + "temp";
+  //   await singleFile.mv(filePath + "\\" + singleFile.name);
+  //   // console.log(singleFile);
 
-    try {
-      console.log("try");
-      if (req.files == undefined) {
-        return res.status(400).send("Please upload an excel file!");
-      }
-      let path = filePath + singleFile.name;
-      console.log(path);
+  //   try {
+  //     console.log("try");
+  //     if (req.files == undefined) {
+  //       return res.status(400).send("Please upload an excel file!");
+  //     }
+  //     let path = filePath + singleFile.name;
+  //     console.log(path);
 
-      readXlsxFile(path).then((rows) => {
-        // skip header
-        rows.shift();
+  //     readXlsxFile(path).then((rows) => {
+  //       // skip header
+  //       rows.shift();
 
-        let users = [];
+  //       let users = [];
 
-        rows.forEach((row) => {
-          let user = {
-            id: row[0],
-            title: row[1],
-            description: row[2],
-            published: row[3],
-          };
+  //       rows.forEach((row) => {
+  //         let user = {
+  //           id: row[0],
+  //           title: row[1],
+  //           description: row[2],
+  //           published: row[3],
+  //         };
 
-          users.push(user);
-        });
+  //         users.push(user);
+  //       });
 
-        User.bulkCreate(users)
-          .then(() => {
-            res.status(200).send({
-              message: "Uploaded the file successfully: " + req.files,
-            });
-          })
-          .catch((error) => {
-            res.status(500).send({
-              message: "Fail to import data into database!",
-              error: error.message,
-            });
-          });
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send({
-        message: "Could not upload the file: " + req.files.name,
-      });
-    }
-  },
+  //       User.bulkCreate(users)
+  //         .then(() => {
+  //           res.status(200).send({
+  //             message: "Uploaded the file successfully: " + req.files,
+  //           });
+  //         })
+  //         .catch((error) => {
+  //           res.status(500).send({
+  //             message: "Fail to import data into database!",
+  //             error: error.message,
+  //           });
+  //         });
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //     res.status(500).send({
+  //       message: "Could not upload the file: " + req.files.name,
+  //     });
+  //   }
+  // },
 };
