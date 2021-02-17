f1e033dde69b6040c83d1d3898a127a44e003f54
niphitphon8bit <niphitphon_nt@hotmail.com>
2021-02-17 21:34:25 +0700

    add function testset for test with test set of problem

--- backend/controllers/ide.controller.js
+++ backend/controllers/ide.controller.js
@@ -38,6 +38,36 @@ module.exports = {
       next(error);
     }
   },
+
+  
+  // function name: testset
+  // description: compile with test set of problem question by API
+  // input: testset of problem, problemid,
+  // output: number of success or error log
+  // CreateBy: Niphitphon Thanatkulkit / CreateDate: 15/2/2021
+  // UpdateBy: Niphitphon Thanatkulkit / UpdateDate: 15/2/2021
+  testset: async (req, res, next) => {
+    const compile = req.query.compile,
+      language = req.query.language,
+      source = req.body.source,
+      path = req.body.path,
+      stdin = req.body.stdin;
+    try {
+      const doesCompile = await comileLogic(
+        language,
+        compile,
+        stdin,
+        source,
+        path
+      );
+
+      res.status(200).send(doesCompile);
+    } catch (error) {
+      if (error.isJoi === true) return next(createError.InternalServerError());
+      next(error);
+    }
+  },
+
   // function name: submit
   // description: submit files by API
   // input: yearName,courseCode,sectionNumber,assignmentTitle,problemTitle,userUsername,taskId,memeFile,singleFile
