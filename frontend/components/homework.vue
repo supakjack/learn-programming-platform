471a9796c437efed8e9784bd48b9274b3308bb03
Theo <theozaza@hotmail.com>
2021-02-18 15:00:46 +0700

    create front ide for student and get api assignments

--- frontend/components/homework.vue
+++ frontend/components/homework.vue
@@ -1,12 +1,12 @@
 <script>
 export default {
   methods: {
-    // function name: getTag
+    // function name: getHomework
     // description: get from backend API
     // input: -
     // output: [doesGetAll]: {tagName, tagId, tagStatus, tagUpdateDate, tagCraeteDate ,CreateName, UpdateName}
-    // CreateBy: Niphitphon Thanatkulkit / CreateDate: 1/2/2021
-    // UpdateBy: Niphitphon Thanatkulkit / UpdateDate: 6/2/2021
+    // CreateBy: Yotsapat Phurahong / CreateDate: 18/2/2021
+    // UpdateBy:
     async getHomework() {
       return this.$axios.$get("homework");
     }
