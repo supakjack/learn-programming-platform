8ec94974e6dbf270cfc4c7c8ac96fc4001fe8071
theo <theozaza@hotmail.com>
2021-02-24 02:13:16 +0700

    run not submit path

--- backend/routers/ide.router.js
+++ backend/routers/ide.router.js
@@ -7,11 +7,6 @@ router.post("/run", ideController.run);
 
 // seperate compile by source or path
 
-// router.post("/seperate", multer().array("files"), function (req, res) {
-//   console.log("body: ", req.body.data.files);
-//   console.log("files:", req.files);
-//   return res.sendStatus(200);
-// });
 router.post("/seperate", ideController.seperate);
 
 // compile by source and path
