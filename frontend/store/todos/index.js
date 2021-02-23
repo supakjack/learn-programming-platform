b07d38d6f0159867d026ec7c381db56a978cca30
niphitphon8bit <niphitphon_nt@hotmail.com>
2021-02-23 18:18:13 +0700

    add vuex to project

---
+++ frontend/store/todos/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+    state: () => ({
+      list: []
+    }),
+    mutations: {
+      add(state, { text }) {
+        state.list.push({
+          text,
+          done: false
+        })
+      },
+      toggle(state, { todo }) {
+        todo.done = !todo.done
+      }
+    }
+  }
