5e57968e8c29f33db5fe7c599e2ae559e51aaa9b
niphitphon8bit <niphitphon_nt@hotmail.com>
2021-02-25 19:18:25 +0700

    update  - vuex     - data getting data

--- frontend/store/course/index.js
+++ frontend/store/course/index.js
@@ -1,40 +1,36 @@
 module.exports = {
   state: () => ({
-    course: [
-      {
-        courseId: null,
-        courseName: null,
-        courseCode: null,
-        courseUpdateDate: null,
+    courseId: null,
+    courseName: null,
+    courseCode: null,
+    courseUpdateDate: null,
 
-        sectionId: null,
-        sectionNumber: null,
-        userId: null,
-        userUsername: null,
+    sectionId: null,
+    sectionNumber: null,
+    userId: null,
+    userUsername: null,
 
-        yearId: null,
-        yearName: null,
-        yearSemester: null
-      }
-    ]
+    yearId: null,
+    yearName: null,
+    yearSemester: null
   }),
   mutations: {
     setCourse(state, { course }) {
-      //   state.courseId = course.courseId;
-      //   state.courseName = course.courseName;
-      //   state.courseCode = course.courseCode;
-      //   state.courseUpdateDate = course.courseUpdateDate;
+      state.courseId = course.courseId;
+      state.courseName = course.courseName;
+      state.courseCode = course.courseCode;
+      state.courseUpdateDate = course.courseUpdateDate;
 
-      //   state.sectionId = course.sectionId;
-      //   state.sectionNumber = course.sectionNumber;
-      //   state.userId = course.userId;
-      //   state.userUsername = course.userUsername;
+      state.sectionId = course.sectionId;
+      state.sectionNumber = course.sectionNumber;
+      state.userId = course.userId;
+      state.userUsername = course.userUsername;
 
-      //   state.yearId = course.yearId;
-      //   state.yearName = course.yearName;
-      //   state.yearSemester = course.yearSemester;
-      state.course.push(course);
-      console.log(course);
+      state.yearId = course.yearId;
+      state.yearName = course.yearName;
+      state.yearSemester = course.yearSemester;
+      // state.course.push(course);
+      // console.log(course);
     }
   }
 };
