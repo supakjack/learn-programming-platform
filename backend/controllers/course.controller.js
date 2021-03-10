24d92ebb5addf6ebebe0ea9bb563b83f02cdbcee
unknown <48548611+supakjack@users.noreply.github.com>
2021-03-10 16:37:54 +0700

    update and delete section
    update and delete section

--- backend/controllers/course.controller.js
+++ backend/controllers/course.controller.js
@@ -9,47 +9,50 @@ const {
 
 module.exports = {
   get: async (req, res, next) => {
-    const getCondition = await getCourseSchema.validateAsync(req.query)
+    const getCondition = await getCourseSchema.validateAsync(req.query);
     try {
       const doesGetSome = await globalModel.select({
-        name: 'users',
+        name: "users",
         condition: [getCondition],
-        whereNot: [{ courseStatus: 'delete' }],
+        whereNot: [{ courseStatus: "delete" }],
         filter: [
-          'userId',
-          'userUsername',
-          'courseId',
-          'courseName',
-          'courseCode',
-          'courseStatus',
-          'courseUpdateDate',
-          'yearId',
-          'yearName',
-          'yearSemester'
+          "userId",
+          "userUsername",
+          "courseId",
+          "courseName",
+          "courseCode",
+          "courseStatus",
+          "courseUpdateDate",
+          "yearId",
+          "yearName",
+          "yearSemester",
         ],
         leftJoin: [
           {
-            joinTable: 'courses',
-            leftTableName: 'users',
-            leftKey: 'userId',
-            joinKey: 'courseCreateBy'
+            joinTable: "courses",
+            leftTableName: "users",
+            leftKey: "userId",
+            joinKey: "courseCreateBy",
           },
           {
-            joinTable: 'years',
-            leftTableName: 'courses',
-            leftKey: 'courseYearId',
-            joinKey: 'yearId'
-          }
-        ]
-      })
+            joinTable: "years",
+            leftTableName: "courses",
+            leftKey: "courseYearId",
+            joinKey: "yearId",
+          },
+        ],
+      });
       const doesGetYearByCreate = await globalModel.select({
-        name: 'years',
-        condition: [{ yearCreateBy: getCondition.userId }]
-      })
-      res.status(200).send({ doesGetSome, doesGetYearByCreate })
+        name: "years",
+        condition: [
+          { yearCreateBy: getCondition.userId },
+          { yearStatus: "active" },
+        ],
+      });
+      res.status(200).send({ doesGetSome, doesGetYearByCreate });
     } catch (error) {
-      if (error.isJoi === true) return next(createError.InternalServerError())
-      next(error)
+      if (error.isJoi === true) return next(createError.InternalServerError());
+      next(error);
     }
   },
   getStudentCourse: async (req, res, next) => {
